71d2f22fdaa97390adfb11a47a108474fe80b272
Arken <Arken.Sarsenov@outlook.com>
2023-12-03 17:18:19 +0500

    Initial Commit

---
+++ HW16_CleanSwift/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HW16_CleanSwift
+//
+//  Created by Arken Sarsenov on 03.12.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
