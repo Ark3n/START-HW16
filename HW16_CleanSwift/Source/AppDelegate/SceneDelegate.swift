73a3cbcb277559a2ec0b8af590da3c6be7da9e5d
Arken <Arken.Sarsenov@outlook.com>
2023-12-03 17:26:26 +0500

    Initial project preparation.

---
+++ HW16_CleanSwift/Source/AppDelegate/SceneDelegate.swift
@@ -0,0 +1,23 @@
+//
+//  SceneDelegate.swift
+//  HW16_CleanSwift
+//
+//  Created by Arken Sarsenov on 03.12.2023.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+
+        guard let scene = (scene as? UIWindowScene) else { return }
+        window = UIWindow(windowScene: scene)
+        window?.rootViewController = ViewController()
+        window?.makeKeyAndVisible()
+    }
+
+}
+
